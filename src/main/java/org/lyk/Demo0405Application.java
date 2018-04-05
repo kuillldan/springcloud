484a37dd0683f8764a5f346d50a0f594e5ae8a5b
Liu <yuankliu@deloitte.com.cn>
2018-04-05 09:01:59 +0800

    asa

---
+++ src/main/java/org/lyk/Demo0405Application.java
@@ -0,0 +1,14 @@
+package org.lyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0405Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Demo0405Application.class, args);
+    }
+}
---
+++ src/main/java/org/lyk/controller/HelloController.java
@@ -0,0 +1,21 @@
+package org.lyk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by yuankliu on 05/04/2018.
+ */
+@Controller
+@RequestMapping("/HelloController/")
+public class HelloController
+{
+    @ResponseBody
+    @RequestMapping("echo")
+    public Object echo(String msg)
+    {
+        System.out.println("收到请求:" + msg);
+        return "你好:" + msg.toUpperCase();
+    }
+}
